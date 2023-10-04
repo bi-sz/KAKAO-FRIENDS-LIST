362d0ec751c1676d55201b0feef9b77bf8d73f64
kwonbi <kwonbi@pumpkinev.co.kr>
2023-10-04 16:43:38 +0900

    추가

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper'
+import Header from './src/Header';
+import Myprofile from './src/Myprofile';
+import { myProfile } from './src/data';
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+
+      <Myprofile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
     </View>
   );
 }
@@ -13,8 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop : statusBarHeight,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const myProfile = {
+    uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/BI2.jpg",
+    name: "비",
+    introduction: "",
+  };
+  
+  export const friendProfiles = [
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Neow.jpg",
+      name: "녜냥",
+      introduction: "",
+    },
+    {
+      uri: "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflowers-3292932__480.jpg",
+      name: "동빈",
+      introduction: "",
+    },
+    {
+      uri: "https://cdn.https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Miseong.png",
+      name: "미성",
+      introduction: "",
+    },
+    {
+      uri: "https://cdn.https.com/https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Jaehyung.png",
+      name: "박재형",
+      introduction: "",
+    },
+    {
+      uri: "https://cdn.pixabay.https://raw.githubusercontent.com/bi-sz/friendsList/master/image/seungsoo.jpg",
+      name: "박승수",
+      introduction: "",
+    },
+    {
+      uri: "https://cdn.https://raw.githubusercontent.com/bi-sz/friendsList/master/image/sonhChu.png",
+      name: "송츄",
+      introduction: "",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/wonhee.jpg",
+      name: "워니",
+      introduction: "삶은.... 계란.",
+    },
+    {
+      uri: "https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_1280.jpg",
+      name: "재민",
+      introduction: "",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Jeongmin.jpeg",
+      name: "정민",
+      introduction: "",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Jeongmin.jpeg",
+      name: "지윤",
+      introduction: "",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Chaewon.jpg",
+      name: "채붕이",
+      introduction: "",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/HyunJi.jpg",
+      name: "현지",
+      introduction: "출근사라져라 뾰로롱-☆",
+    },
+    {
+      uri: "https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Hojin.jpg",
+      name: "호진",
+      introduction: "",
+    },
+  ];
